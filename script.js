904fabdb3bc772f93665cc7fe74df0750e836677
Gabriele Bonfim <gabriele_bonfim@outlook.com>
2021-02-18 21:28:04 -0500

    Adicionado script do calculo do IMC, com resultado final via alert

---
+++ script.js
@@ -0,0 +1,14 @@
+var inputPeso = document.querySelector("#peso");
+var inputAltura = document.querySelector("#altura");
+
+
+function calcula(){
+    var peso = parseFloat(inputPeso.value.replace(",","."));
+    var altura = parseFloat(inputAltura.value.replace(",","."));
+
+    var calculo = peso / (Math.pow(altura, 2));
+    var resultado = calculo;
+    alert(parseFloat(resultado.toFixed(1)));
+}
+var button = document.querySelector("#calcular");
+button.onclick = calcula;
